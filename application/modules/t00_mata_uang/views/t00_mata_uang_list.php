65921be9fb796d1e21704a753363f846e41166dd
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2023-08-11 11:32:44 +0700

    .

--- application/modules/t00_mata_uang/views/t00_mata_uang_list.php
+++ application/modules/t00_mata_uang/views/t00_mata_uang_list.php
@@ -53,20 +53,15 @@ $this->load->view('dist/_partials/header');
                     <div class="col">
                         <!-- Page pre-title -->
                         <div class="page-pretitle">
-                            Overview
+                            List
                         </div>
                         <h2 class="page-title">
-                            Dashboard
+                            Mata Uang
                         </h2>
                     </div>
                     <!-- Page title actions -->
                     <div class="col-auto ms-auto d-print-none">
                         <div class="btn-list">
-                            <span class="d-none d-sm-inline">
-                                <a href="#" class="btn">
-                                    New view
-                                </a>
-                            </span>
                             <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
                                 <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
@@ -89,9 +84,9 @@ $this->load->view('dist/_partials/header');
 
                     <div class="col-12">
                         <div class="card">
-                            <div class="card-header">
+                            <!-- <div class="card-header">
                                 <h3 class="card-title">Invoices</h3>
-                            </div>
+                            </div> -->
                             <div class="card-body border-bottom py-3">
                                 <div class="d-flex">
                                     <div class="text-muted">
@@ -138,9 +133,10 @@ $this->load->view('dist/_partials/header');
                                 </table>
                             </div>
                             <div class="card-footer d-flex align-items-center">
-                                <div class="col-md-6">
+                                <!-- <div class="col-md-6">
                                     <a href="#" class="btn btn-primary">Total Record : <?php echo $total_rows ?></a>
-                                </div>
+                                </div> -->
+                                <p class="m-0 text-muted">Total Record <span><?php echo $total_rows ?></span></p>
                                 <div class="col-md-6 text-right">
                                     <?php echo $pagination ?>
                                 </div>
