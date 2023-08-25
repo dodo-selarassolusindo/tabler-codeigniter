57453df9d1c81126443925611c270dde0da74317
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2023-08-25 10:20:03 +0700

    .

--- application/modules/t35_selisih_bayar/views/t35_selisih_bayar_w_search.php
+++ application/modules/t35_selisih_bayar/views/t35_selisih_bayar_w_search.php
@@ -29,26 +29,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         <table class="table card-table table-vcenter text-nowrap datatable">
                             <tr>
                                 <th>No</th>
-                                <th>Bkm Detail</th>
+                                <!-- <th>Bkm Detail</th> -->
                                 <th>Tanggal</th>
-                                <th>Mata Uang</th>
+                                <!-- <th>Mata Uang</th> -->
                                 <th>Jumlah</th>
                                 <th>Proses Data</th>
                             </tr>
                             <?php foreach ($t35_selisih_bayar_data as $t35_selisih_bayar) { ?>
                             <tr>
-                                <td width="80px"><?= ++$start ?></td>
-                                <td><?= $t35_selisih_bayar->bkm_detail ?></td>
-                                <td><?= $t35_selisih_bayar->tanggal ?></td>
-                                <td><?= $t35_selisih_bayar->mata_uang ?></td>
-                                <td><?= $t35_selisih_bayar->jumlah ?></td>
-                                <td style="text-align:center" width="200px">
+                                <td><?= ++$start ?></td>
+                                <!-- <td><?= $t35_selisih_bayar->bkm_detail ?></td> -->
+                                <td><?= date_dmy($t35_selisih_bayar->tanggal) ?></td>
+                                <!-- <td><?= $t35_selisih_bayar->mata_uang ?></td> -->
+                                <td><?= $t35_selisih_bayar->mata_uang.' '.number_format($t35_selisih_bayar->jumlah, 0) ?></td>
+                                <td>
                                 <?php
-                                echo anchor(site_url('t35_selisih_bayar/read/'.$t35_selisih_bayar->id),'Detail');
-                                echo ' | ';
-                                echo anchor(site_url('t35_selisih_bayar/update/'.$t35_selisih_bayar->id),'Ubah');
-                                echo ' | ';
-                                echo anchor(site_url('t35_selisih_bayar/delete/'.$t35_selisih_bayar->id),'Hapus','onclick="javasciprt: return confirm(\'Hapus data ?\')"');
+                                echo anchor(site_url('t35_selisih_bayar/read/'.$t35_selisih_bayar->id),'Detail','class="btn btn-primary btn-sm"');
+                                echo ' ';
+                                echo anchor(site_url('t35_selisih_bayar/update/'.$t35_selisih_bayar->id),'Ubah','class="btn btn-primary btn-sm"');
+                                echo ' ';
+                                echo anchor(site_url('t35_selisih_bayar/delete/'.$t35_selisih_bayar->id),'Hapus','class="btn btn-danger btn-sm" onclick="javasciprt: return confirm(\'Hapus data ?\')"');
                                 ?>
                                 </td>
                             </tr>
