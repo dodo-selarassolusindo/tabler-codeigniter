b34a6be9b94b6e5017073cc92bf452eb547c8275
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2023-08-31 13:23:06 +0700

    .

--- application/modules/t33_pembayaran/views/t33_pembayaran_wo_search_2.php
+++ application/modules/t33_pembayaran/views/t33_pembayaran_wo_search_2.php
@@ -2,28 +2,25 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-                    <div class="row row-cards">
-                        <div class="col-md-1">
-                            <div class="mb-3">
-                                <!-- <label class="form-label">No</label> -->
-                                <?= $start ?>
+                            <div class="row row-cards">
+                                <div class="col-md-1">
+                                    <div class="mb-3">
+                                        <?= $start ?>
+                                    </div>
+                                </div>
+                                <div class="col-md-11">
+                                    <div class="mb-3">
+                                        <select class="form-control select2" name="bkm_detail[]" multiple="multiple">
+                                            <?php foreach($t31_bkm_detail_all_data as $row) { ?>
+                                            <option value="<?= $row->id ?>"><?= $row->name ?></option>
+                                            <?php } ?>
+                                        </select>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-11">
-                            <div class="mb-3">
-                                <!-- <label class="form-label">Name</label> -->
-                                <select class="form-control select2" name="bkm_detail[]" multiple="multiple">
-                                    <?php foreach($t31_bkm_detail_all_data as $row) { ?>
-                                    <option value="<?= $row->id ?>"><?= $row->name ?></option>
-                                    <?php } ?>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
 
-                </div>
-                <div class="card-footer ">
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                </div>
-                </form>
-                <!-- </div> -->
+                        </div>
+                        <div class="card-footer ">
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </div>
+                    </form>
