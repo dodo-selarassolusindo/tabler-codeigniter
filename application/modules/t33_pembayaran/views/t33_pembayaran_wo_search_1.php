86e503a5ac2346034bf308da735af22d247fdf03
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2023-08-31 13:17:54 +0700

    .

--- application/modules/t33_pembayaran/views/t33_pembayaran_wo_search_1.php
+++ application/modules/t33_pembayaran/views/t33_pembayaran_wo_search_1.php
@@ -1,40 +1,44 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-                    <div class="table-responsive">
-                        <table class="table card-table table-vcenter text-nowrap datatable">
-                            <tr>
-                                <th>No</th>
-                                <th>Name</th>
-                                <th>Mata Uang</th>
-                                <th>Jumlah</th>
-                                <!-- <th>Proses Data</th> -->
-                            </tr>
-                            <?php //foreach ($t33_pembayaran_data as $t33_pembayaran) { ?>
-                            <?php $t33_pembayaran = $t33_pembayaran_data ?>
-                            <tr>
-                                <td><?= $start ?></td>
-                                <td><?= $this->T31_bkm_detail_model->get_by_id($t33_pembayaran->bkm_detail)->name ?></td>
-                                <td>
-                                    <select class="form-control select2" name="mata_uang">
-                                    <?php foreach($this->T00_mata_uang_model->get_all() as $row) { ?>
-                                        <option value="<?= $row->id ?>" <?= $row->id == $t33_pembayaran->mata_uang ? 'selected' : '' ?>><?= $row->kode ?></option>
-                                    <?php } ?>
-                                    </select>
-                                </td>
-                                <td>
-                                    <input type="text" style="display: inline; width: auto;" class="form-control" name="jumlah" id="jumlah" value="<?= $t33_pembayaran->jumlah ?>" />
-                                </td>
-                                <!-- <td> -->
-                                <?php
-                                // echo anchor(site_url('t33_pembayaran/read/'.$t33_pembayaran->id),'Detail');
-                                // echo ' | ';
-                                // echo anchor(site_url('t33_pembayaran/update/'.$t33_pembayaran->id),'Ubah');
-                                // echo ' | ';
-                                // echo anchor(site_url('t33_pembayaran/delete/'.$t33_pembayaran->id),'Hapus','onclick="javasciprt: return confirm(\'Hapus data ?\')"');
-                                ?>
-                                <!-- </td> -->
-                            </tr>
-                            <?php //} ?>
-                        </table>
-                    </div>
+                    <?php $t33_pembayaran = $t33_pembayaran_data ?>
+                    <!-- <div class="col-12"> -->
+                        <form class="card">
+                            <div class="card-body">
+                                <!-- <h3 class="card-title">Edit Profile</h3> -->
+                                <div class="row row-cards">
+                                    <div class="col-md-1">
+                                        <div class="mb-3">
+                                            <label class="form-label">No</label>
+                                            <?= $start ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <div class="mb-3">
+                                            <label class="form-label">Name</label>
+                                            <?= $this->T31_bkm_detail_model->get_by_id($t33_pembayaran->bkm_detail)->name ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <div class="mb-3">
+                                            <label class="form-label">Mata Uang</label>
+                                            <select class="form-control select2" name="mata_uang">
+                                            <?php foreach($this->T00_mata_uang_model->get_all() as $row) { ?>
+                                                <option value="<?= $row->id ?>" <?= $row->id == $t33_pembayaran->mata_uang ? 'selected' : '' ?>><?= $row->kode ?></option>
+                                            <?php } ?>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <div class="mb-3">
+                                            <label class="form-label">Jumlah</label>
+                                            <input type="text" class="form-control" name="jumlah" id="jumlah" value="<?= $t33_pembayaran->jumlah ?>" />
+                                        </div>
+                                    </div>
+                                </div>
+                            <!-- </div>
+                            <div class="card-footer text-end">
+                                <button type="submit" class="btn btn-primary">Update Profile</button>
+                            </div>
+                        </form>
+                    </div> -->
--- application/modules/t33_pembayaran/views/t33_pembayaran_wo_search_2.php
+++ application/modules/t33_pembayaran/views/t33_pembayaran_wo_search_2.php
@@ -1,39 +1,29 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-                    <div class="table-responsive">
-                        <table class="table card-table table-vcenter text-nowrap datatable">
-                            <tr>
-                                <td><?= $start ?></td>
-                                <td>
-                                    <select class="form-control select2" name="bkm_detail[]" multiple="multiple">
-                                        <?php foreach($t31_bkm_detail_all_data as $row) { ?>
-                                        <option value="<?= $row->id ?>"><?= $row->name ?></option>
-                                        <?php } ?>
-                                    </select>
-                                </td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                                <td>&nbsp;</td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="container text-center">
-                        <div class="row">
-                            <div class="col">
-                                Column
+
+                    <div class="row row-cards">
+                        <div class="col-md-1">
+                            <div class="mb-3">
+                                <!-- <label class="form-label">No</label> -->
+                                <?= $start ?>
                             </div>
-                            <div class="col">
-                                Column
-                            </div>
-                            <div class="col">
-                                Column
+                        </div>
+                        <div class="col-md-11">
+                            <div class="mb-3">
+                                <!-- <label class="form-label">Name</label> -->
+                                <select class="form-control select2" name="bkm_detail[]" multiple="multiple">
+                                    <?php foreach($t31_bkm_detail_all_data as $row) { ?>
+                                    <option value="<?= $row->id ?>"><?= $row->name ?></option>
+                                    <?php } ?>
+                                </select>
                             </div>
                         </div>
                     </div>
+
+                </div>
+                <div class="card-footer ">
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+                </form>
+                <!-- </div> -->
